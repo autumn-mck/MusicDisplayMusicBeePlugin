cff69774358bab6b1e7d57459d81b141e4ddd2f7
Autumn McKee <autumn@mck.is>
2024-02-26 21:58:16 +0000

    Send post request with currently playing data

--- PostPublicMusicPlugin.cs
+++ PostPublicMusicPlugin.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Drawing;
 using System.Windows.Forms;
-using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace MusicBeePlugin
 {
     public partial class Plugin
     {
         private MusicBeeApiInterface _mbApiInterface;
-        private readonly PluginInfo _about = new PluginInfo();
+        private readonly PluginInfo _about = new();
 
         public PluginInfo Initialise(IntPtr apiInterfacePtr)
         {
@@ -39,7 +38,7 @@ namespace MusicBeePlugin
         public bool Configure(IntPtr panelHandle)
         {
             // save any persistent settings in a sub-folder of this path
-            var dataPath = _mbApiInterface.Setting_GetPersistentStoragePath();
+            // var dataPath = _mbApiInterface.Setting_GetPersistentStoragePath();
 
             // panelHandle will only be set if you set about.ConfigurationPanelHeight to a non-zero value
             // keep in mind the panel width is scaled according to the font the user has selected
@@ -51,11 +50,9 @@ namespace MusicBeePlugin
             prompt.AutoSize = true;
             prompt.Location = new Point(0, 0);
             prompt.Text = "prompt:";
-            using (var textBox = new TextBox())
-            {
-                textBox.Bounds = new Rectangle(60, 0, 100, textBox.Height);
-                configPanel.Controls.AddRange(new Control[] { prompt, textBox });
-            }
+            using var textBox = new TextBox();
+            textBox.Bounds = new Rectangle(60, 0, 100, textBox.Height);
+            configPanel.Controls.AddRange([prompt, textBox]);
 
             return false;
         }
@@ -65,7 +62,7 @@ namespace MusicBeePlugin
         public void SaveSettings()
         {
             // save any persistent settings in a sub-folder of this path
-            var dataPath = _mbApiInterface.Setting_GetPersistentStoragePath();
+            // var dataPath = _mbApiInterface.Setting_GetPersistentStoragePath();
         }
 
         // MusicBee is closing the plugin (plugin is being disabled by user or MusicBee is shutting down)
@@ -83,23 +80,34 @@ namespace MusicBeePlugin
         public void ReceiveNotification(string sourceFileUrl, NotificationType type)
         {
             // perform some action depending on the notification type
-            switch (type)
+            if (type is not (NotificationType.TrackChanged or NotificationType.PlayStateChanged)) return;
+
+
+            var artist = _mbApiInterface.NowPlaying_GetFileTag(MetaDataType.Artist);
+            var title = _mbApiInterface.NowPlaying_GetFileTag(MetaDataType.TrackTitle);
+            var album = _mbApiInterface.NowPlaying_GetFileTag(MetaDataType.Album);
+            var durationMs = _mbApiInterface.NowPlaying_GetDuration();
+            var positionMs = _mbApiInterface.Player_GetPosition();
+            var playState = _mbApiInterface.Player_GetPlayState();
+
+            var albumArt = _mbApiInterface.NowPlaying_GetArtwork();
+
+            var json = new
             {
-                case NotificationType.PluginStartup:
-                    // perform startup initialisation
-                    switch (_mbApiInterface.Player_GetPlayState())
-                    {
-                        case PlayState.Playing:
-                        case PlayState.Paused:
-                            // ...
-                            break;
-                    }
-                    break;
-                case NotificationType.TrackChanged:
-                    var artist = _mbApiInterface.NowPlaying_GetFileTag(MetaDataType.Artist);
-                    // ...
-                    break;
-            }
+                artist,
+                title,
+                album,
+                durationMs,
+                positionMs,
+                playState,
+
+                albumArt
+            };
+
+            // Send the JSON object to the server 
+            var client = new System.Net.WebClient();
+            client.Headers.Add("Content-Type", "application/json");
+            client.UploadString("http://localhost:3000/now-playing", "POST", JsonConvert.SerializeObject(json));
         }
 
         // return an array of lyric or artwork provider names this plugin supports
